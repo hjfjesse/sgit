456393a097c67b21b169ae4410443ad3801ac754
hjfjesse <360461748@qq.com>
2019-03-21 18:08:57 +0800

    fix must_install.sh

--- Linux/must_install.sh
+++ Linux/must_install.sh
@@ -1,22 +1,23 @@
 #####最小化安装CENTOS7后，下一步的操作#########
 
-##安装必备的包
+
+
 yum install -y net-tools bash-completion vim-enhanced wget lrzsz htop  gcc gcc-c++
 yum install -y epel-release
 
 ##关闭SELINUX 和 防火墙
 setenforce 0
 systemctl stop firewalld
-systemctl disable firewalld.service 
+systemctl disable firewalld.service
 
 
 
 #进入源的目录 
-cd /etc/yum.repo.d 
+cd /etc/yum.repos.d/
 mkdir repo_bak
 mv *.repo repo_bak
 #下载阿里源,163源 
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo 
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget http://mirrors.163.com/.help/CentOS7-Base-163.repo
 
 #清除系统yum缓存并生成新的缓存
@@ -24,5 +25,4 @@ yum clean all && yum makecache
 yum repolist
 
 sleep 10
-
 
